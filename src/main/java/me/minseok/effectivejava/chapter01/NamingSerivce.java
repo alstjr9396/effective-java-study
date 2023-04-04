d3d527fc66dbc4d057d03caeb4257c0aabb5260c
minseok.kang <minseokkang95@gmail.com>
2023-04-04 23:40:53 +0900

    chapter01: 인터페이스와 정적 메서드

--- src/main/java/me/minseok/effectivejava/chapter01/NamingSerivce.java
+++ src/main/java/me/minseok/effectivejava/chapter01/NamingSerivce.java
@@ -1,6 +1,26 @@
 package me.minseok.effectivejava.chapter01;
 
+/**
+ * 자바 9부터 private 정적 메서드를 가질 수 있다.
+ * 모든 구현체가 해당 메소드를 정의할 필요가 없다.(강제성이 없다.)
+ */
 public interface NamingSerivce {
 
     public String naming();
+
+    static private void prepareMessage() {}
+
+    static public String randomNaming() {
+        prepareMessage();
+        return "randomName";
+    }
+
+    static public String randomNumericNaming() {
+        prepareMessage();
+        return "randomNumbericName";
+    }
+
+    default int getMaxNameSize() {
+        return 10;
+    }
 }
