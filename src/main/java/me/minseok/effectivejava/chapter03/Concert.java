25892599da0d09ceef77539afd498b2125ad2d8a
minseok.kang <minseokkang95@gmail.com>
2023-04-11 23:08:23 +0900

    chapter02: 메서드 참조 및 함수형 인터페이스

--- src/main/java/me/minseok/effectivejava/chapter03/Concert.java
+++ src/main/java/me/minseok/effectivejava/chapter03/Concert.java
@@ -1,5 +1,10 @@
 package me.minseok.effectivejava.chapter03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Concert {
@@ -36,6 +41,34 @@ public class Concert {
 
     public static void main(String[] args) {
         Concert concert = new Concert(Elvis.INSTANCE);
+
+        // 메서드 참조: 메서드 하나만 호출하는 람다 표현식을 줄여쓰는 방법
+        concert.start(() -> SingerImpl.getInstance());
         concert.start(SingerImpl::getInstance);
+
+        // 함수형 인터페이스: 람다 표현식과 메서드 참조에 대한 타겟 타입을 제공
+
+        // Output만 있는 함수형 인터페이스
+        Supplier<Singer> singerSupplier = SingerImpl::getInstance;
+        concert.start(singerSupplier);
+
+        // Input과 Output이 있는 함수형 인터페이스
+        Function<Singer, String> stringFunction = Object::toString;
+        System.out.println(stringFunction);
+
+        // Input만 잇는 함수형 인터페이스
+        List<String> nameList = new ArrayList<>();
+        nameList.add("John");
+        nameList.add("Tom");
+        Consumer<String> singerConsumer = SingerImpl::sayName;
+        nameList.forEach(singerConsumer);
+
+        // Output 타입이 boolean인 함수형 인터페이스
+        List<SingerImpl> singerList = new ArrayList<>();
+        singerList.add(SingerImpl.getInstance());
+        singerList.add(SingerImpl.getInstance());
+        Predicate<SingerImpl> singerPredicate = SingerImpl::isPro;
+        System.out.println(singerList.stream().filter(singerPredicate).count());
+
     }
 }
--- src/main/java/me/minseok/effectivejava/chapter03/SingerImpl.java
+++ src/main/java/me/minseok/effectivejava/chapter03/SingerImpl.java
@@ -1,8 +1,11 @@
 package me.minseok.effectivejava.chapter03;
 
+
 public class SingerImpl implements Singer {
 
     private static final SingerImpl INSTANCE = new SingerImpl();
+
+    private static String level = "pro";
 
     private SingerImpl() {}
 
@@ -13,5 +16,17 @@ public class SingerImpl implements Singer {
     @Override
     public void sing() {
         System.out.println("Hello!");
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public static void sayName(String name) {
+        System.out.println(name);
+    }
+
+    public static boolean isPro(SingerImpl singer) {
+        return singer.getLevel().equals("pro");
     }
 }
