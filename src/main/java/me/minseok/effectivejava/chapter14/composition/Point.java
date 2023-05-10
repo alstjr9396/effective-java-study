54a0e81b78c6d3e53aff4b9aecbf33936e2f86a2
minseok.kang <minseokkang95@gmail.com>
2023-05-10 23:01:41 +0900

    chapter14: Comparable을 구현할지 고민하라

---
+++ src/main/java/me/minseok/effectivejava/chapter14/composition/Point.java
@@ -0,0 +1,20 @@
+package me.minseok.effectivejava.chapter14.composition;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Point point) {
+        int result = Integer.compare(this.x, point.x);
+        if (result == 0) {
+            result = Integer.compare(this.y, point.y);
+        }
+        return result;
+    }
+}
